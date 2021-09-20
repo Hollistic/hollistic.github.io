f2f2c96a4a63bc1c8099e0b9011ca60c0cd6aba4
Umair Khan <umairk2019@gmail.com>
2021-09-20 09:56:10 -0600

    ball scene part 1

--- ball-scene-demo/sketch.js
+++ ball-scene-demo/sketch.js
@@ -1,15 +1,50 @@
-// Project Title
-// Your Name
-// Date
+// Ball Scene Demo
+// Umair Khan
+// September 20th, 2021
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let ballArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 1000; i++) {
+    let ball = {
+      x: random(width),
+      y: random(height),
+      dx: random(3, 5),
+      dy: random(3, 5),
+      radius:  random(10, 30),
+      ballColor: color(random(255), random(255), random(255), random(255)),
+    };
+    ballArray.push(ball);
+  }
 }
 
 function draw() {
   background(220);
+  
+  moveBall();
+  displayBall(); 
 }
+
+function moveBall() {
+  for (let theBall of ballArray) {
+    theBall.x += theBall.dx;
+    theBall.y += theBall.dy;
+    theBall.dx = random(-7, 7);
+    theBall.dy = random(-7, 7);
+  }
+}
+
+
+function displayBall() {
+  for (let theBall of ballArray) {
+    noStroke();
+    fill(theBall.ballColor);
+    ellipse(theBall.x, theBall.y, theBall.radius*2);
+  }
+}
+
+
