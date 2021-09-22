461f3e320c3777b280badebd43f0d80b7b7e2d14
Umair Khan <umairk2019@gmail.com>
2021-09-22 11:01:59 -0600

    Added terrain generation

--- terrain-generation/sketch.js
+++ terrain-generation/sketch.js
@@ -1,15 +1,37 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Terrain Generation
+// Umair Khan
+// 9/22/2021
 
+let heights = [];
+let numberOfRectangles;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  numberOfRectangles = width;
+  generateTerrain(numberOfRectangles);
 }
 
 function draw() {
   background(220);
+  displayTerrain();
+}
+
+
+function displayTerrain() {
+  let rectWidth = width/numberOfRectangles;
+  fill("black");
+  for (let i = 0; i < heights.length; i++){
+    rect(i*rectWidth, height-heights[i], rectWidth, heights[i]);
+  }
+}
+
+function generateTerrain(howMany) {
+  let time = 0;
+  let dTime = 0.002;
+  
+  for (let i = 0; i < howMany; i++) {
+    let currentHeight = noise(time)*height;
+    heights.push(currentHeight);
+    time += dTime;
+  }
 }
