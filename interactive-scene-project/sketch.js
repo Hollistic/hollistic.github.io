f06640a08d8b4a700d387aebcfebc0e08c4cc9f9
Umair Khan <umairk2019@gmail.com>
2021-09-26 17:03:57 -0600

    Finished interactive scene project
    added all music/sound effects, door mouse functionality added, added win

--- interactive-scene-project/sketch.js
+++ interactive-scene-project/sketch.js
@@ -5,22 +5,27 @@
 //
 // Extra for Experts:
 // - created my own shapes using polygons
-// - added sound effects
+// - added music/sound effects
+//
+// Instructions:
+// Use WASD to control your player try and get to the end of the level.
+// Press the mouse to open the door.
+// Have fun!
 
-//to-do: Need to fix collision for bottom of door, add mouse for door, add music, and create full level.
 
 
 //Level Values
 let level = 1; 
 let startPositionX;
 let startPositionY;
+let winGame;
 
 //Player Values
-let playerX = 1; 
-let playerY = 1; 
+let playerX = 20; 
+let playerY = 20; 
 let playerW = 10; 
 let playerH = 10; 
-let speed = 2;
+let speed = 1.5;
 
 //Screen Border
 const border = []; // stores the vertices of the screen border polygon
@@ -31,24 +36,29 @@ let wallPoly2 = [];
 
 //door
 let door = [];
-let hitDoor = false;
 let doorOpen = false;
 
 //Collison
 let hit = false;
+let hitDoor = false;
+let touchEnd = false;
 
-//music
-let music;
+//music and sound effects
+let musicLoop;
+let playerExplodesSound;
+let winGameSound;
 
-
+//preload assets
 function preload() {
-  music = loadSound("assets/gigakoops-level-1-fire-mountain.mp3");
+  musicLoop = loadSound("assets/caveloop.wav");
+  playerExplodesSound = loadSound("assets/explode.wav");
+  winGameSound = loadSound("assets/win.wav");
 }
 
-
-
+//setup
 function setup() {
   createCanvas(600, 400);
+  winGame = false;
 
   //define the vertices of the canvas border
   border[0] = createVector(0,0);
@@ -57,12 +67,12 @@ function setup() {
   border[3] = createVector(0, height);
 
   //start music loop
-  music.loop();
+  musicLoop.loop();
 }
 
 //main draw loop
 function draw() {
-  background(220);
+  background(170);
   
   checkLevel();
   findLocation();
@@ -99,12 +109,28 @@ function wasd() {
   if (keyIsDown(68)) {
     playerX += speed;
   }
-  
 }
 
+//checks which level we are on and sets an according start position for the player
 function checkLevel() {
   if (level === 1) {
     startPositionX = 20; startPositionY = 20; 
+
+    //makes rectangle in bottom right corner for winning the game
+    push();
+    noStroke();
+    fill("limegreen");
+    rect(width-50, height-50, 50, 50);
+    pop();
+  }
+  
+  //text appears when game is won
+  if (winGame) {
+    push();
+    fill("limegreen");
+    textSize(50);
+    text("You Win!", 10, 40);
+    pop();
   }
 }
 
@@ -117,78 +143,114 @@ function drawPlayer(){
   
 }
 
+//draws the main walls of the level
 function drawWalls(){
   if (level === 1){
-    //define the vertices of the wall polygon
-    wallPoly1[0] = createVector(1, 50);
+    //define the vertices of the wall polygon 1
+    wallPoly1[0] = createVector(0, 50);
     wallPoly1[1] = createVector(170, 50);
-    wallPoly1[2] = createVector(281, 120);
-    wallPoly1[3] = createVector(1, 80);
+    wallPoly1[2] = createVector(300, 150);
+    wallPoly1[3] = createVector(0, 200);
 
+    //wallpoly2
+    wallPoly2[0] = createVector(200, 0);
+    wallPoly2[1] = createVector(width, 0);
+    wallPoly2[2] = createVector(width, 116);
+    wallPoly2[3] = createVector(355, 116);
     
+
     // Draw the polygon by iterating over 4 created vectors x/y stored in wall[]:
     push();
-    beginShape();
     fill(244, 144, 9);
     noStroke();
+
+    //wallpoly1
+    beginShape();
     for (let {x, y} of wallPoly1)  {
       vertex(x, y);
     }
     endShape(CLOSE);
-    pop();
-  }
-}
 
-function drawDoor() {
-  door[0] = createVector(200, 50); 
-  door[1] = createVector(250, 50);
-  door[2] = createVector(250, 100);
-  door[3] = createVector(200, 100);
-
-  push();
-  beginShape();
-  fill("grey");
-  for (let {x, y} of door)  {
-    vertex(x, y);
-  }
-  endShape(CLOSE);
-  pop();
+    //wallpoly2
+    beginShape();
+    for (let {x, y} of wallPoly2)  {
+      vertex(x, y);
+    }
+    endShape(CLOSE);
   
+    //rectangle walls
+    rect(0, 150, 560, 60);
+    rect(25, 237, width, 80);
+    rect(0, 335, 520, height);
+    pop();
 
-
+  }
 }
 
-function playerCollision() {
-  // hit is true if player touches the wall or screen border polygon
-  hit = collideRectPoly(playerX, playerY, playerW, playerH, wallPoly1) || collideRectPoly(playerX, playerY, playerW, playerH, border);
-  hitDoor = collideRectPoly(playerX, playerY, playerW, playerH, door);
+//draws the door in the level
+function drawDoor() {
+  if (level === 1) {
+    
+    // define vertices of door polygon
+    door[0] = createVector(250, 200); 
+    door[1] = createVector(275, 200);
+    door[2] = createVector(275, 250);
+    door[3] = createVector(250, 250);
 
-  //if player hits a wall or border return to start position
+    //draw polygon if mouse isn't pressed otherwise don't draw it
+    push();
+    if (!mouseIsPressed){ 
+      beginShape();
+      fill("magenta");
+      for (let {x, y} of door)  {
+        vertex(x, y);
+      }
+      endShape(CLOSE);
+      pop();
+    }
+  }
+}
+
+
+// checks for player collision with border, walls or door
+function playerCollision() {
+  // if the player x/y collides with these objects at their x/y positions the statement is set to true
+  hit = collideRectPoly(playerX, playerY, playerW, playerH, border) || collideRectPoly(playerX, playerY, playerW, playerH, wallPoly1) || collideRectPoly(playerX, playerY, playerW, playerH, wallPoly2) || collideRectRect(playerX, playerY, playerW, playerH, 0, 150, 560, 60) || collideRectRect(playerX, playerY, playerW, playerH, 25, 237, width, 80) || collideRectRect(playerX, playerY, playerW, playerH, 0, 335, 520, height);
+  hitDoor = collideRectPoly(playerX, playerY, playerW, playerH, door);
+  winGame = collideRectRect(playerX, playerY, playerW, playerH, width-50, height-50, 50, 50);
+
+  //if player hits a wall/ border return to start position
   if (hit){
+    playerExplodesSound.play();
     playerX = startPositionX; playerY = startPositionY;
   }
 
   //checks if touching door
   if (hitDoor){
-    for (let {x, y} of door){
-      if (playerY <= door[1].y) {
-        playerY -= 0.5;
-        console.log("hit from top");
-      }
-      
-      else if (playerY >= door[2].y) {
-        playerY += 0.5;
-        console.log("hit from bottom");
-      }
+    if(!mouseIsPressed){
+      for (let {x, y} of door){
+        //the player keeps getting pushed back 25% of their speed in x or y directions if they try to touch the door
+        let pushBack = speed * (25/100);
 
-      else if (playerX <= door[0].x) {
-        playerX -= 0.5;
-        console.log("hit from left");
-      }
+        if (playerY <= door[1].y) {
+          playerY -= pushBack;
+          console.log("hit from top");
+        }
 
-      else if (playerX >= door[3].x) {
-        playerX += 0.5;
-        console.log("hit from right");
+        else if (playerY >= door[2].y) {
+          playerY += pushBack;
+          console.log("hit from bottom");
+        }
+
+        else if (playerX <= door[0].x) {
+          playerX -= pushBack;
+          console.log("hit from left");
+        }
+
+        else if (playerX >= door[3].x) {
+          playerX += pushBack;
+          console.log("hit from right");
+        }
       }
     }
   }
