facb5e668781fb6c3c1a04bdaac9fe8445c8e86e
Umair Khan <umairk2019@gmail.com>
2021-10-05 11:00:52 -0600

    updated to-do list, made sure window resized to scale at all times,

--- minesweeper/sketch.js
+++ minesweeper/sketch.js
@@ -2,27 +2,37 @@
 // Umair Khan
 // 9/28/2021
 //
-// Extras for experts/to do
-// implemented flood fill algorithm
-// implemented audio
-// main menu and general polish
+// To-Do
+// -random bomb location grid and another playing grid with blanks on top of that
+// -number indications adjacent to bombs in NESW
+// -flood fill algorithm when pressing blank space, clear all adjacent blank spaces
+// -game lost when mine pressed
+// -mine flagging
+// -different levels of difficulties
+// -polishing by adding music, sprites, menu, etc
 
 let gridSize = 10;
 let grid;
 let cellSize;
 let gameLost = false;
 
+//setup
 function setup() {
+  //creates largest square possible
   if (windowHeight < windowWidth) {
-    createCanvas(windowHeight, windowHeight);
+    createCanvas(windowHeight*0.9, windowHeight*0.9);
   }
   else {
-    createCanvas(windowWidth, windowWidth);
+    createCanvas(windowWidth*0.9, windowWidth*0.9);
   }
   
   grid = createRandomArray(gridSize);
 
   cellSize = width / gridSize;
+}
+
+function windowResized() {
+  setup();
 }
 
 function draw() {
@@ -36,14 +46,14 @@ function mousePressed() {
     let cellX = Math.floor(mouseX/cellSize); 
     let cellY = Math.floor(mouseY/cellSize); 
 
-    blankSpaceClicked(cellX, cellY);
+    uncoveredSquareClicked(cellX, cellY);
   }
 }
 
-//clicked blank
-function blankSpaceClicked(x, y) {
+//clicked uncovered square
+function uncoveredSquareClicked(x, y) {
   if (x>=0 && x<gridSize && y>=0 && y<gridSize) {
-    if (grid[y][x] === 0){
+    if (grid[y][x] === 10){
       grid[y][x] = 1;
     }
     if (grid[y][x] === "bomb") {
@@ -59,7 +69,7 @@ function createRandomArray(howLarge) {
     newArray.push([]);
     for(let x = 0; x < howLarge; x++) {
       if (random(0, 100) > 30){
-        newArray[y].push(0);
+        newArray[y].push(10);
       }
       else {
         newArray[y].push("bomb");
@@ -69,14 +79,36 @@ function createRandomArray(howLarge) {
   return newArray;
 }
 
+function countAdjacentBomb() {
+
+  //current cell [y][x]
+
+  //north [y-1][x]
+
+  //east [y][x+1]
+
+  //south [y+1][x]
+
+  //west [y][x-1]
+
+  //north-east [y-1][x+1]
+
+  //south-east [y+1][x+1]
+  
+  //north-west [y-1][x-1]
+
+  //south-west [y+1][x-1]
+
+}
+
 function displayGrid() {
   for (let y=0; y < gridSize; y++) {
     for (let x=0; x < gridSize; x++) {
       //uncovered
-      if (grid[y][x] === 0){
+      if (grid[y][x] === 10){
         fill("grey");
       }
-      //revealed
+      //empty
       else if (grid[y][x] === 1) {
         fill("white");
       }
@@ -86,6 +118,7 @@ function displayGrid() {
       }
       rect(x*cellSize, y*cellSize, cellSize, cellSize);
     }
+
   }
 }
 
