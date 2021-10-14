42fb7465daf22b84f164636f99b9a3fe28308658
Umair Khan <umairk2019@gmail.com>
2021-10-13 20:43:18 -0600

    Update sketch.js

--- minesweeper/sketch.js
+++ minesweeper/sketch.js
@@ -2,7 +2,7 @@
 // Umair Khan
 // 9/28/2021
 //
-// To-Do
+// To-Do/wants
 // -random bomb location grid and another playing grid with blanks on top of that
 // -number indications adjacent to bombs in NESW
 // -flood fill algorithm when pressing blank space, clear all adjacent blank spaces
@@ -33,22 +33,24 @@ function setup() {
     createCanvas(windowWidth*0.9, windowWidth*0.9);
   }
   
-  grid = createRandomArray(gridSize);
+  grid = createArray(gridSize);
 
   cellSize = width / gridSize;
 
-  countAdjacentBomb();
 }
 
+//on window resize
 function windowResized() {
   setup();
 }
 
+//main draw loop
 function draw() {
   background(220);
 
   displayGrid();
 }
+
 
 function mousePressed() {
   if(mouseX <= width && mouseY <= height){
@@ -56,6 +58,12 @@ function mousePressed() {
     let cellY = Math.floor(mouseY/cellSize); 
 
     uncoveredSquareClicked(cellX, cellY);
+  }
+}
+
+class Cell {
+  obstructor() {
+
   }
 }
 
@@ -72,7 +80,7 @@ function uncoveredSquareClicked(x, y) {
 }
 
 
-function createRandomArray(howLarge) {
+function createArray(howLarge) {
   let newArray = [];
   for (let y = 0; y < howLarge; y++) {
     newArray.push([]);
@@ -88,49 +96,49 @@ function createRandomArray(howLarge) {
   return newArray;
 }
 
-function countAdjacentBomb() {
+// function countAdjacentBomb() {
 
-  for (let y=0; y<gridSize; y++) {
-    for (let x=0; x<gridSize; x++) {
-      // current cell [y][x]
-      if (grid[y][x] === "*") {
-        //sanity checks are created for each individual adjacent square to check if it's outside of the array or a bomb
-        if (y-1 > 0 && y-1 !== "*") {
-          grid[y-1][x] += 1; //north [y-1][x]
-        }
-        if (x+1 < gridSize[y] && x+1 !== "*") {
-          grid[y][x+1] += 1; //east [y][x+1]
-        }
-        if (y+1 < gridSize && y+1 !== "*") {        
-          grid[y+1][x] += 1; //south [y+1][x]
-        }
-        if (x-1 > 0 && x-1 !== "*") {
-          grid[y][x-1] += 1; //west [y][x-1]
-        }
-        if (y-1 > 0 && x+1 < gridSize[y] && y-1 !== "*" && x+1 !== "*") {
-          grid[y-1][x+1] += 1; //north-east [y-1][x+1]
-        }
-        if (y+1 < gridSize && x+1 < gridSize[y] && y+1 !== "*" && x+1 !== "*") {
-          grid[y+1][x+1] += 1; //south-east [y+1][x+1]
-        }
-        if (y-1 > 0 && x-1 > 0 && y-1 !== "*" && x-1 !== "*") {
-          grid[y-1][x-1] += 1; //north-west [y-1][x-1]
-        }
-        if (y+1 < gridSize && x-1 > 0 && y+1 !== "*" && x-1 !== "*") {
-          grid[y+1][x-1] += 1; //south-west [y+1][x-1]
-        }
-      }
-    }
-  }
-}
+//   for (let y=0; y<gridSize; y++) {
+//     for (let x=0; x<gridSize; x++) {
+//       // current cell [y][x]
+//       if (grid[y][x] === "*") {
+//         //sanity checks are created for each individual adjacent square to check if it's outside of the array or a bomb
+//         if (y-1 > 0 && y-1 !== "*") {
+//           grid[y-1][x] += 1; //north [y-1][x]
+//         }
+//         if (x+1 < gridSize[y] && x+1 !== "*") {
+//           grid[y][x+1] += 1; //east [y][x+1]
+//         }
+//         if (y+1 < gridSize && y+1 !== "*") {        
+//           grid[y+1][x] += 1; //south [y+1][x]
+//         }
+//         if (x-1 > 0 && x-1 !== "*") {
+//           grid[y][x-1] += 1; //west [y][x-1]
+//         }
+//         if (y-1 > 0 && x+1 < gridSize[y] && y-1 !== "*" && x+1 !== "*") {
+//           grid[y-1][x+1] += 1; //north-east [y-1][x+1]
+//         }
+//         if (y+1 < gridSize && x+1 < gridSize[y] && y+1 !== "*" && x+1 !== "*") {
+//           grid[y+1][x+1] += 1; //south-east [y+1][x+1]
+//         }
+//         if (y-1 > 0 && x-1 > 0 && y-1 !== "*" && x-1 !== "*") {
+//           grid[y-1][x-1] += 1; //north-west [y-1][x-1]
+//         }
+//         if (y+1 < gridSize && x-1 > 0 && y+1 !== "*" && x-1 !== "*") {
+//           grid[y+1][x-1] += 1; //south-west [y+1][x-1]
+//         }
+//       }
+//     }
+//   }
+// }
 
-function drawNumbers() {
-  for (let y=0; y<gridSize; y++) {
-    for (let x=0; x<gridSize; x++) {
-      fill("black");
-    }
-  }
-}
+// function drawNumbers() {
+//   for (let y=0; y<gridSize; y++) {
+//     for (let x=0; x<gridSize; x++) {
+//       fill("black");
+//     }
+//   }
+// }
 
 function displayGrid() {
   for (let y=0; y < gridSize; y++) {
